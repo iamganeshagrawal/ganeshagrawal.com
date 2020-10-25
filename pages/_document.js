0353d3b14139edee1afb79f7788332b643de5790
Ganesh Agrawal <gmegotiya@gmail.com>
2020-10-25 18:26:19 +0530

    Design upgrade started

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 // TODO: Add meta tags for seo
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html lang="en" prefix="og: https://ogp.me/ns#" style={{ "--vh": '100%' }}>
+      <Html
+        lang="en"
+        prefix="og: https://ogp.me/ns#"
+        style={{ "--vh": "100%" }}
+      >
         <Head>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
           <meta name="author" content="Ganesh Agrawal" />
-          <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css' />
-          <link rel="stylesheet" href="/assets/css/normalize.css" media="screen" />
-          <link rel="stylesheet" href="/assets/css/simple-grid.css" media="screen" />
+          <link
+            href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic"
+            rel="stylesheet"
+            type="text/css"
+          />
           <link rel="stylesheet" href="/assets/css/style.css" media="screen" />
         </Head>
         <body>
@@ -25,8 +31,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,50 @@
-import CustomHead from '../components/CustomHead';
-import Intro, { sectionList } from '../components/section/Intro';
-import About from '../components/section/About'
-import { useState, useRef, useEffect } from 'react';
-
-const inBtw = (value, min, max) => {
-    return (value > min && value < max)
-}
+import CustomHead from "../components/CustomHead";
 
 const Home = () => {
-    const [section, setSection] = useState(null);
-    useEffect(() => {
-        console.log(section)
-    })
-    return (
-        <>
-            <CustomHead title="Ganesh Agrawal - Software Engineer" />
-            <Intro activeSection={section} />
-            {/* <About /> */}
-            <SectionDiv id={sectionList[0]} color="#91d18b" onScreen={setSection} firstSection />
-            <SectionDiv id={sectionList[1]} color="#ea5455" onScreen={setSection} />
-            <SectionDiv id={sectionList[2]} color="#776d8a" onScreen={setSection} />
-            <SectionDiv id={sectionList[3]} color="#3c2c3e" onScreen={setSection} />
-            <SectionDiv id={sectionList[4]} color="#1b262c" onScreen={setSection} />
-            <SectionDiv id={sectionList[5]} color="#158467" onScreen={setSection} />
-        </>
-    )
-}
-
-const SectionDiv = ({ id, color, onScreen, firstSection = false }) => {
-    const ref = useRef(null);
-    const handleScroll = () => {
-        if (ref.current) {
-            const { top, bottom } = ref.current.getBoundingClientRect();
-            const windowHeight = window.innerHeight;
-            if (firstSection && inBtw(top, windowHeight * 0.75, windowHeight)) {
-                onScreen(null);
-            }
-            if (inBtw(top, 0, windowHeight * 0.25) || inBtw(bottom, windowHeight * 0.75, windowHeight)) {
-                // console.log(id);
-                onScreen(id);
-            }
-        }
-    }
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        }
-    }, [])
-    return (
-        <div ref={ref} id={id} style={{ height: '100vh', backgroundColor: color, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <h1>{id}</h1>
+  return (
+    <>
+      <CustomHead title="Ganesh Agrawal - Software Engineer" />
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1>Ganesh Agrawal</h1>
+        <img
+          src="/assets/svg/maintenance.svg"
+          alt="maintenance mode"
+          style={{ width: "256px" }}
+        />
+        <h2>Hi, I’m Ganesh. I’m a Full Stack Web Developer.</h2>
+        <div>
+          <p>
+            We are currently upgrading this website.. So you can checkout my{" "}
+            <a href="https://blog.ganeshgrawal.com">blogs</a>.
+          </p>
         </div>
-    )
-}
+        <p>
+          Feel free to contact me at{" "}
+          <a
+            href="https://www.linkedin.com/in/iamganeshagrawal"
+            title="Ganesh Agrawal | LinkedIn Profile"
+          >
+            LinkedIn
+          </a>{" "}
+          //{" "}
+          <a
+            href="https://twitter.com/imganeshagrawal"
+            title="Ganesh Agrawal | Twitter Profile"
+          >
+            Twitter
+          </a>{" "}
+          // <a href="mailto:hello@ganeshgrawal.com">hello@ganeshgrawal.com</a>!
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Home;
